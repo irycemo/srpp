623d97ff3e2a6e8ec26520069d1f2ab40d3415d4
kikorobledo <enrique_j_@hotmail.com>
2024-04-02 12:26:50 -0600

    Fix  render propiedad

--- app/Livewire/Inscripciones/Propiedad/PropiedadIndex.php
+++ app/Livewire/Inscripciones/Propiedad/PropiedadIndex.php
@@ -22,32 +22,60 @@ class PropiedadIndex extends Component
     public function render()
     {
 
-        $movimientos = MovimientoRegistral::with('inscripcionPropiedad', 'asignadoA', 'actualizadoPor', 'folioReal')
-                                                ->whereHas('folioReal', function($q){
-                                                    $q->where('estado', 'activo');
-                                                })
-                                                ->where(function($q){
-                                                    $q->whereHas('asignadoA', function($q){
-                                                            $q->where('name', 'LIKE', '%' . $this->search . '%');
-                                                        })
-                                                        ->orWhere('solicitante', 'LIKE', '%' . $this->search . '%')
-                                                        ->orWhere('tomo', 'LIKE', '%' . $this->search . '%')
-                                                        ->orWhere('registro', 'LIKE', '%' . $this->search . '%')
-                                                        ->orWhere('distrito', 'LIKE', '%' . $this->search . '%')
-                                                        ->orWhere('seccion', 'LIKE', '%' . $this->search . '%')
-                                                        ->orWhere('tramite', 'LIKE', '%' . $this->search . '%');
-                                                })
-                                                ->when(auth()->user()->ubicacion == 'Regional 4', function($q){
-                                                    $q->where('distrito', 2);
-                                                })
-                                                ->when(auth()->user()->ubicacion != 'Regional 4', function($q){
-                                                    $q->where('distrito', '!=', 2);
-                                                })
-                                                ->whereHas('inscripcionPropiedad', function($q){
-                                                    $q->whereIn('servicio', ['D158', 'D122', 'D114', 'D125', 'D126', 'D124', 'D121', 'D120', 'D119', 'D123', 'D113', 'D115', 'D116', 'D118']);
-                                                })
-                                                ->orderBy($this->sort, $this->direction)
-                                                ->paginate($this->pagination);
+
+        if(auth()->user()->hasRole(['Propiedad'])){
+
+            $movimientos = MovimientoRegistral::with('inscripcionPropiedad', 'asignadoA', 'actualizadoPor', 'folioReal')
+                                                    ->whereHas('folioReal', function($q){
+                                                        $q->where('estado', 'activo');
+                                                    })
+                                                    ->where(function($q){
+                                                        $q->whereHas('asignadoA', function($q){
+                                                                $q->where('name', 'LIKE', '%' . $this->search . '%');
+                                                            })
+                                                            ->orWhere('solicitante', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('tomo', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('registro', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('distrito', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('seccion', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('tramite', 'LIKE', '%' . $this->search . '%');
+                                                    })
+                                                    ->when(auth()->user()->ubicacion == 'Regional 4', function($q){
+                                                        $q->where('distrito', 2);
+                                                    })
+                                                    ->when(auth()->user()->ubicacion != 'Regional 4', function($q){
+                                                        $q->where('distrito', '!=', 2);
+                                                    })
+                                                    ->whereHas('inscripcionPropiedad', function($q){
+                                                        $q->whereIn('servicio', ['D158', 'D122', 'D114', 'D125', 'D126', 'D124', 'D121', 'D120', 'D119', 'D123', 'D113', 'D115', 'D116', 'D118']);
+                                                    })
+                                                    ->orderBy($this->sort, $this->direction)
+                                                    ->paginate($this->pagination);
+
+        }elseif(auth()->user()->hasRole(['Administrador'])){
+
+            $movimientos = MovimientoRegistral::with('inscripcionPropiedad', 'asignadoA', 'actualizadoPor', 'folioReal')
+                                                    ->whereHas('folioReal', function($q){
+                                                        $q->where('estado', 'activo');
+                                                    })
+                                                    ->where(function($q){
+                                                        $q->whereHas('asignadoA', function($q){
+                                                                $q->where('name', 'LIKE', '%' . $this->search . '%');
+                                                            })
+                                                            ->orWhere('solicitante', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('tomo', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('registro', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('distrito', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('seccion', 'LIKE', '%' . $this->search . '%')
+                                                            ->orWhere('tramite', 'LIKE', '%' . $this->search . '%');
+                                                    })
+                                                    ->whereHas('inscripcionPropiedad', function($q){
+                                                        $q->whereIn('servicio', ['D158', 'D122', 'D114', 'D125', 'D126', 'D124', 'D121', 'D120', 'D119', 'D123', 'D113', 'D115', 'D116', 'D118']);
+                                                    })
+                                                    ->orderBy($this->sort, $this->direction)
+                                                    ->paginate($this->pagination);
+
+        }
 
         return view('livewire.inscripciones.propiedad.propiedad-index', compact('movimientos'))->extends('layouts.admin');
     }
