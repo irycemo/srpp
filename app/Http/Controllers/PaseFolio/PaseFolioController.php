cc7a6dc1d9897c77a108b3656c1a4d724911d859
kikorobledo <enrique_j_@hotmail.com>
2024-07-10 16:10:55 -0600

    Fix gravamenes pase afolio

--- app/Http/Controllers/PaseFolio/PaseFolioController.php
+++ app/Http/Controllers/PaseFolio/PaseFolioController.php
@@ -15,7 +15,7 @@ class PaseFolioController extends Controller
 
     public function caratula(FolioReal $folioReal){
 
-        $folioReal->load('predio', 'antecedentes', 'gravamenes.deudores.persona', 'gravamenes.deudores.actor.persona', 'gravamenes.acreedores.persona', 'sentencias', 'varios', 'cancelaciones.movimientoRegistral');
+        $folioReal->load('predio', 'antecedentes', 'gravamenes.deudores.persona', 'gravamenes.deudores.actor.persona', 'gravamenes.acreedores.persona', 'gravamenes.movimientoRegistral', 'sentencias.movimientoRegistral', 'varios.movimientoRegistral', 'cancelaciones.movimientoRegistral');
 
         $formatter = new NumeroALetras();
 
