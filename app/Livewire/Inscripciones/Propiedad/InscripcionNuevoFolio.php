ee53ea006fe9d83e95644c30bcf5bff9316fad8c
kikorobledo <enrique_j_@hotmail.com>
2024-10-29 12:24:18 -0600

    Fix pase a folio colindancias repetidas

---
+++ app/Livewire/Inscripciones/Propiedad/InscripcionNuevoFolio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Inscripciones\Propiedad;
+
+use Livewire\Component;
+
+class InscripcionNuevoFolio extends Component
+{
+    public function render()
+    {
+        return view('livewire.inscripciones.propiedad.inscripcion-nuevo-folio');
+    }
+}
