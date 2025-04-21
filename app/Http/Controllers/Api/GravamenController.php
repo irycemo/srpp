2177272f6506cbe1e08bcdf04652bd5f11b8a6f7
kikorobledo <enrique_j_@hotmail.com>
2025-04-21 13:09:48 -0600

    Consulta g

--- app/Http/Controllers/Api/GravamenController.php
+++ app/Http/Controllers/Api/GravamenController.php
@@ -29,7 +29,7 @@ class GravamenController extends Controller
                                                         })
                                                         ->first();
 
-                                                        info($data);
+                                                        info($data, $movimientoRegistral);
 
         if(!$movimientoRegistral){
 
