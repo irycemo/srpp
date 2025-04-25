92328246168a81d34935d58d309cad8841966479
kikorobledo <enrique_j_@hotmail.com>
2025-04-25 15:55:18 -0600

    Fix caratula propiedad

--- resources/views/incripciones/propiedad/acto.blade.php
+++ resources/views/incripciones/propiedad/acto.blade.php
@@ -265,57 +265,7 @@
 
             @endif
 
-            @if(count($propiedad->propietarios))
-
-                <p class="separador">propietarios</p>
-
-                <table>
-
-                    <thead>
-
-                        <tr>
-                            <th style="padding-right: 10px;">Nombre / Razón social</th>
-                            <th style="padding-right: 10px;">% de propiedad</th>
-                            <th style="padding-right: 10px;">% de nuda</th>
-                            <th style="padding-right: 10px;">% de usufructo</th>
-                        </tr>
-
-                    </thead>
-
-                    <tbody>
-
-                        @foreach ($propiedad->propietarios as $propietario)
-
-                            <tr>
-                                <td style="padding-right: 40px;">
-                                    <p style="margin:0">{{ $propietario->nombre }} {{ $propietario->ap_paterno }} {{ $propietario->ap_materno }} {{ $propietario->razon_social }}</p>
-                                    @if($propietario->multiple_nombre)
-                                        <p style="margin:0">({{ $propietario->multiple_nombre }})</p>
-                                    @endif
-                                    @if(isset($propietario->representado_por))
-
-                                        <strong>representado(a) por: </strong>{{ $propietario->representado_por }}
-
-                                    @endif
-                                </td>
-                                <td style="padding-right: 40px;">
-                                    <p style="margin:0">{{ $propietario->porcentaje_propiedad ?? '0.00' }} %</p>
-                                </td>
-                                <td style="padding-right: 40px;">
-                                    <p style="margin:0">{{ $propietario->porcentaje_nuda ?? '0.00' }} %</p>
-                                </td>
-                                <td style="padding-right: 40px;">
-                                    <p style="margin:0">{{ $propietario->porcentaje_usufructo ?? '0.00' }} %</p>
-                                </td>
-                            </tr>
-
-                        @endforeach
-
-                    </tbody>
-
-                </table>
-
-            @endif
+            @include('comun.caratulas.propietarios')
 
             <p class="parrafo">
                 A SOLICITUD DE: <strong>{{ $datos_control->solicitante }}</strong> se EXPiDe EL PRESENTE EN LA CIUDAD DE @if($datos_control->distrito == '02 Uruapan' ) uruapan @else MORELIA @endif, MICHOACÁN, A LAS {{ $datos_control->elaborado_en }}.
