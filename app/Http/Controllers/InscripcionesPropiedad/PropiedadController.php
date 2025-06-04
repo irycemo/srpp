467ba4d15639687001a569cb5ca5cb7f671d6dcc
kikorobledo <enrique_j_@hotmail.com>
2025-06-04 14:14:56 -0600

    Fix caratula propiedad

--- app/Http/Controllers/InscripcionesPropiedad/PropiedadController.php
+++ app/Http/Controllers/InscripcionesPropiedad/PropiedadController.php
@@ -51,7 +51,7 @@ class PropiedadController extends Controller
         $datos_control->monto = $propiedad->movimientoRegistral->monto;
         $datos_control->tipo_servicio = $propiedad->movimientoRegistral->tipo_servicio;
         $datos_control->asigno_folio = $propiedad->movimientoRegistral->folioReal->asignado_por;
-        $datos_control->fecha_prelacion = $propiedad->fecha_prelacion;
+        $datos_control->fecha_prelacion = $propiedad->movimientoRegistral->fecha_prelacion;
 
         $object = (object)[];
 
