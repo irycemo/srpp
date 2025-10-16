7567cd1d46247d5e32d612baa0277206250c756d
kikorobledo <enrique_j_@hotmail.com>
2025-10-16 14:06:29 -0600

    Fix pdf

--- app/Models/FolioReal.php
+++ app/Models/FolioReal.php
@@ -146,6 +146,8 @@ class FolioReal extends Model implements Auditable
 
         if(app()->isProduction()){
 
+            info("entra");
+
             info($this->archivos()->where('descripcion', 'documento_entrada')->latest()->first());
 
             if($this->archivos()->where('descripcion', 'documento_entrada')->latest()->first()){
