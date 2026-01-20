bec26254ea73b79c47481adad6a488f5c61bc15a
kikorobledo <enrique_j_@hotmail.com>
2026-01-20 14:43:20 -0600

    Sea grego funcion para intercalar folio simplificado y pase a folio

--- resources/views/livewire/pase-folio/pase-folio-simplificado/pase-folio-simplificado.blade.php
+++ resources/views/livewire/pase-folio/pase-folio-simplificado/pase-folio-simplificado.blade.php
@@ -367,7 +367,7 @@
                                             wire:loading.attr="disabled"
                                             class="w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                             role="menuitem">
-                                            Enviar a folio simplificado
+                                            Enviar a pase a folio
                                         </button>
 
                                     </div>
