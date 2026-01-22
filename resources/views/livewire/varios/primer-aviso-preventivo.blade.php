56ae4640359d8d9ad5beff571d014d1939ef4601
kikorobledo <enrique_j_@hotmail.com>
2026-01-22 13:57:15 -0600

    Fix varios

--- resources/views/livewire/varios/primer-aviso-preventivo.blade.php
+++ resources/views/livewire/varios/primer-aviso-preventivo.blade.php
@@ -148,56 +148,7 @@
 
     </x-dialog-modal>
 
-    <x-dialog-modal wire:model="modalDocumento" maxWidth="sm">
-
-        <x-slot name="title">
-
-            Subir archivo
-
-        </x-slot>
-
-        <x-slot name="content">
-
-            <x-filepond::upload wire:model="documento" :accepted-file-types="['application/pdf']"/>
-
-            <div>
-
-                @error('documento') <span class="error text-sm text-red-500">{{ $message }}</span> @enderror
-
-            </div>
-
-        </x-slot>
-
-        <x-slot name="footer">
-
-            <div class="flex gap-3">
-
-                <x-button-blue
-                    wire:click="guardarDocumento"
-                    wire:loading.attr="disabled"
-                    wire:target="guardarDocumento">
-
-                    <img wire:loading wire:target="guardarDocumento" class="mx-auto h-4 mr-1" src="{{ asset('storage/img/loading3.svg') }}" alt="Loading">
-
-                    <span>Guardar</span>
-
-                </x-button-blue>
-
-                <x-button-red
-                    wire:click="$toggle('modalDocumento')"
-                    wire:loading.attr="disabled"
-                    wire:target="$toggle('modalDocumento')"
-                    type="button">
-
-                    <span>Cerrar</span>
-
-                </x-button-red>
-
-            </div>
-
-        </x-slot>
-
-    </x-dialog-modal>
+    @include('livewire.comun.inscripciones.modal-guardar_documento_entrada_pdf')
 
     @filepondScripts
 
