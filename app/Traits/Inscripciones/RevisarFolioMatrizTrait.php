fb83651587e78801f2f9a64ba002646f09f97046
kikorobledo <enrique_j_@hotmail.com>
2026-01-28 12:40:55 -0600

    Fix folio matriz

--- app/Traits/Inscripciones/RevisarFolioMatrizTrait.php
+++ app/Traits/Inscripciones/RevisarFolioMatrizTrait.php
@@ -40,7 +40,7 @@ trait RevisarFolioMatrizTrait
             $nuevoMovimientoRegistral->numero_propiedad = null;
             $nuevoMovimientoRegistral->estado = 'concluido';
             $nuevoMovimientoRegistral->servicio_nombre = 'Genera nuevo folio real';
-            $nuevoMovimientoRegistral->folio = $movimiento->folioReal->ultimoFolio();
+            $nuevoMovimientoRegistral->folio = $movimiento->folioReal->ultimoFolio() + 1;
             $nuevoMovimientoRegistral->save();
 
             Propiedad::create([
