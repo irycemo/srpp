7fa371d8159bc2ab0e879dac90713f874ea44240
kikorobledo <enrique_j_@hotmail.com>
2026-02-01 13:28:26 -0600

    Fix provider

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Providers;
+
+use Livewire\Livewire;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+
+        Model::shouldBeStrict();
+
+        if(app()->isProduction()){
+
+            URL::forceScheme('https');
+
+            Livewire::setScriptRoute(function ($handle) {
+                return Route::get('/srpp/public/vendor/livewire/livewire.js', $handle);
+            });
+
+            Livewire::setUpdateRoute(function ($handle) {
+                return Route::post('/srpp/livewire/update', $handle);
+            });
+
+        }elseif(app()->environment('staging')){
+
+            Livewire::setScriptRoute(function ($handle) {
+                return Route::get('/srpp/public/vendor/livewire/livewire.js', $handle);
+            });
+
+            Livewire::setUpdateRoute(function ($handle) {
+                return Route::post('/srpp/livewire/update', $handle);
+            });
+
+        }
+
+    }
+}
