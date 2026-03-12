b644f52a6c9a4b43efe075af9b6b4e522fef6245
kikorobledo <enrique_j_@hotmail.com>
2026-03-12 16:01:45 -0600

    Fix request actualizar

--- app/Http/Requests/MovimientoRegistralUpdateRequest.php
+++ app/Http/Requests/MovimientoRegistralUpdateRequest.php
@@ -35,6 +35,7 @@ class MovimientoRegistralUpdateRequest extends FormRequest
            /* Tramite */
            'tipo_servicio' => 'nullable',
            'categoria_servicio' => 'required',
+           'servicio' => 'nullable',
 
            /* Solicitante */
            'solicitante' => 'nullable',
