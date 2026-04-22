de8eb3ce0b862b1306e7e159c47162c0e82d2633
root <root@DESKTOP-IVHFQKO.localdomain>
2026-04-22 15:16:43 -0600

    Fix pase a folio

--- app/Livewire/PaseFolio/PaseFolio.php
+++ app/Livewire/PaseFolio/PaseFolio.php
@@ -172,7 +172,7 @@ class PaseFolio extends Component
 
                         $this->modelo_editar->update(['estado' => 'concluido']);
 
-                        $this->modelo_editar->porpiedad->update(['descripcion_acto' => 'ESTE MOVIMIENTO REGISTRAL DA ORIGEN A ESTE FOLIO REAL.']);
+                        $this->modelo_editar->inscripcionPropiedad->update(['descripcion_acto' => 'ESTE MOVIMIENTO REGISTRAL DA ORIGEN A ESTE FOLIO REAL.']);
 
                     }
 
