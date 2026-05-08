bd1aaea780dd8a901607d5bb35080006207430c2
root <root@DESKTOP-IVHFQKO.localdomain>
2026-05-08 16:17:56 -0600

    Fix finalizar persona moral

--- app/Livewire/PersonaMoral/ReformasIndex.php
+++ app/Livewire/PersonaMoral/ReformasIndex.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Log;
 use App\Traits\Inscripciones\InscripcionesIndex;
 use App\Exceptions\InscripcionesServiceException;
 use App\Traits\Inscripciones\EnviarMovimientoCorreccion;
+use App\Traits\Inscripciones\FinalizarInscripcionTrait;
 use App\Traits\Inscripciones\ReasignarUsuarioTrait;
 use App\Traits\Inscripciones\RechazarMovimientoTrait;
 use App\Traits\Inscripciones\RecibirDocumentoTrait;
@@ -29,6 +30,7 @@ class ReformasIndex extends Component
     use EnviarMovimientoCorreccion;
     use RecibirDocumentoTrait;
     use ReasignarUsuarioTrait;
+    use FinalizarInscripcionTrait;
 
     public function estaBloqueado(){
 
