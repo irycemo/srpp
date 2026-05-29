1f8ab1e9ea06d5103c29ce7d2aa6d3df236f8386
root <root@DESKTOP-IVHFQKO.localdomain>
2026-05-29 08:57:48 -0600

    Fix console

--- routes/console.php
+++ routes/console.php
@@ -112,3 +112,146 @@ Artisan::command('pase_a_folio', function(){
     info("Proceso actualizar condicion de pase a folio finalizado.");
 
 });
+
+Artisan::command('desgloses', function(){
+
+    $count = 0;
+
+    $folios_reales = FolioReal::with('predio.escritura')
+                        ->whereIn('folio', [
+                            18202,
+                            18213,
+                            18217,
+                            18219,
+                            18221,
+                            18222,
+                            18225,
+                            18230,
+                            18231,
+                            18232,
+                            18233,
+                            18234,
+                            18238,
+                            18239,
+                            18240,
+                            18241,
+                            18242,
+                            18243,
+                            18244,
+                            18245,
+                            18246,
+                            18247,
+                            18249,
+                            18250,
+                            18251,
+                            18252,
+                            18253,
+                            18255,
+                            18256,
+                            18257,
+                            18258,
+                            18260,
+                            18261,
+                            18262,
+                            18263,
+                            18264,
+                            18265,
+                            18266,
+                            18267,
+                            18268,
+                            18269,
+                            18270,
+                            18271,
+                            18272,
+                            18273,
+                            18274,
+                            18275,
+                            18276,
+                            18277,
+                            18278,
+                            18279,
+                            18280,
+                            18281,
+                            18282,
+                            18283,
+                            18284,
+                            18285,
+                            18286,
+                            18287,
+                            18288,
+                            18289,
+                            18290,
+                            18291,
+                            18292,
+                            18293,
+                            18294,
+                            18295,
+                            18296,
+                            18297,
+                            18298,
+                            18299,
+                            18300,
+                            18301,
+                            18302,
+                            18303,
+                            18304,
+                            18305,
+                            18306,
+                            18307,
+                            18308,
+                            18309,
+                            18310,
+                            18311,
+                            18312,
+                            18313,
+                            18314,
+                            18315,
+                            18316,
+                            18317,
+                            18318,
+                            18319,
+                            18320,
+                            18321,
+                            18322,
+                            18323,
+                            18324,
+                            18325,
+                            18326,
+                            18327,
+                            18328,
+                            18329,
+                            18330,
+                            18331,
+                            18332,
+                            18333,
+                            18334,
+                            18335,
+                        ])
+                        ->get();
+
+    $progressbar = $this->output->createProgressBar($folios_reales->count());
+
+    $progressbar->start();
+
+    foreach($folios_reales as $folio_real){
+
+        try {
+
+            $folio_real->predio->escritura->update(['estado_notario', 'MICHOACÁN']);
+
+            $progressbar->advance();
+
+            $count ++;
+
+        } catch (\Throwable $th) {
+            $this->info($th);
+
+        }
+
+    }
+
+    $progressbar->finish();
+
+    $this->info($count);
+
+});
