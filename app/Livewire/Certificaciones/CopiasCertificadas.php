65c2363a7db2c14509dae2f0a8eddcfccf6a3e3b
root <root@DESKTOP-IVHFQKO.localdomain>
2026-06-08 13:19:49 -0600

    Fix copias

--- app/Livewire/Certificaciones/CopiasCertificadas.php
+++ app/Livewire/Certificaciones/CopiasCertificadas.php
@@ -410,7 +410,7 @@ class CopiasCertificadas extends Component
 
     public function imprimirCaratulaMovimiento(Certificacion $modelo){
 
-        $movimientoRegistral = $modelo->movimientoRegistral->folioReal->movimientosRegistrales()->where('folio', $modelo->movimiento_registral->folio)->first();
+        $movimientoRegistral = $modelo->movimientoRegistral->folioReal->movimientosRegistrales()->where('folio', $modelo->movimientoRegistral->folio)->first();
 
         try {
 
