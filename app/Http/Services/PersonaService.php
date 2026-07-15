177ef6d65284d09d7abb7e44d863d99793d8e77f
root <root@DESKTOP-IVHFQKO.localdomain>
2026-07-15 16:07:09 -0600

    Fix propietarios

--- app/Http/Services/PersonaService.php
+++ app/Http/Services/PersonaService.php
@@ -11,31 +11,36 @@ class PersonaService{
 
         $persona = null;
 
-        if($rfc){
+        if(isset($rfc)){
 
             $persona = Persona::where('rfc', $rfc)->first();
 
-        }elseif($curp){
+            if($persona) return $persona;
+
+        }
+
+        if(isset($curp)){
 
             $persona = Persona::where('curp', $curp)->first();
 
-        }else{
-
-            if($tipo_persona == 'FISICA'){
-
-                $persona = Persona::query()
-                            ->where('nombre', $nombre)
-                            ->where('ap_paterno', $ap_paterno)
-                            ->where('ap_materno', $ap_materno)
-                            ->first();
-
-            }else{
-
-                $persona = Persona::where('razon_social', 'like', '%' . $razon_social . '%')->first();
-
-            }
+            if($persona) return $persona;
 
         }
+
+        $persona = Persona::query()
+                        ->when($nombre, function($q)use ($nombre){
+                            $q->where('nombre', trim($nombre));
+                        })
+                        ->when($ap_paterno, function($q) use ($ap_paterno){
+                            $q->where('ap_paterno', trim($ap_paterno));
+                        })
+                        ->when($ap_materno, function($q) use ($ap_materno){
+                            $q->where('ap_materno', trim($ap_materno));
+                        })
+                        ->when($razon_social, function($q) use ($razon_social){
+                            $q->where('razon_social', trim($razon_social));
+                        })
+                        ->first();
 
         return $persona;
 
