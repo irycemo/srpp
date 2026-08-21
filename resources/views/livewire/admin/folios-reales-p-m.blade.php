c7195d87923814a23a8ab1642f3a8009e8f336af
root <root@DESKTOP-IVHFQKO.localdomain>
2026-08-21 14:46:53 -0600

    FIx constatnes

--- resources/views/livewire/admin/folios-reales-p-m.blade.php
+++ resources/views/livewire/admin/folios-reales-p-m.blade.php
@@ -153,7 +153,7 @@
 
                                         <button
                                             wire:click="borrarFolioReal({{ $folio->id }})"
-                                            wire:confirm="¿Esta seguro que desea borar el folio real de persona moral? Todos sus movimientos registrales seran eliminados, la información no podrá ser recuperada."
+                                            wire:confirm="¿Esta segurso que desea borar el folio real de persona moral? Todos sus movimientos registrales seran eliminados, la información no podrá ser recuperada."
                                             wire:loading.attr="disabled"
                                             class="w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                             role="menuitem">
